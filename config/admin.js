bdc5ee2058f01f8c6d1c2258907332e0c969b64b
Jeremy Gouveia <avak1908@gmail.com>
2023-07-21 11:48:16 -0400

    added TRANSFER_TOKEN_SALT

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,9 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'b4dbad35-6caa-4f84-be56-90d0d3bc2fdf'),
   },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'thisisthetransferkccms')
+    }
+  }
 });
